d67bbdcc2cad2cea2170494019b667ece18f5ec7
venkatbalaji <venkatb2206@gmail.com>
2020-04-12 10:34:02 +0530

    Created Rouer API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const postRouter = require("./routers/postRouter");
+const authorRouter = require("./routers/authorRouter");
+const adminRouter = require("./routers/adminRouter");
 const app = express();
 
 app.set("trust proxy", 1); // trust first proxy
@@ -9,6 +11,10 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Blog Backend running!");
 });
+
+app.use("/post", postRouter);
+app.use("/authors", authorRouter);
+app.use("/admin", adminRouter);
 
 const server = app.listen(8080, () => {
   console.log("Server running on port: " + server.address().port);
